659be7fa1cceae09cd129b10aa9ef10e7365bba1
Blaž Bočko <blaz.bocko1@gmail.com>
2024-09-07 20:50:10 +0200

    Implement clickable star icon

--- src/app/shared/recipe-card/recipe-card.component.ts
+++ src/app/shared/recipe-card/recipe-card.component.ts
@@ -1,14 +1,41 @@
-import { Component, input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RecipeDetails } from '../../recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltip,
+    NgStyle,
+  ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.css',
 })
 export class RecipeCardComponent {
-  title = input<string>();
-  image = input<string>();
-  readyInMinutes = input<number>();
+  recipe = input.required<Partial<RecipeDetails>>();
+
+  iconClicked = output<number>();
+
+  onIconClick() {
+    this.iconClicked.emit(this.recipe().id!);
+  }
+
+  getTooltipMessage() {
+    return this.recipe().isFavorite
+      ? 'Remove from favorites'
+      : 'Add to favorites';
+  }
+
+  getStarColor() {
+    return this.recipe().isFavorite ? 'gold' : 'black';
+  }
 }
